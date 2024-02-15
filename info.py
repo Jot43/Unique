9c82dcc1f2466da2f6826c12abef28ab51c8f8b6
Jot43 <139986366+Jot43@users.noreply.github.com>
2024-02-15 08:00:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -53,8 +53,8 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 BOT_USERNAME = environ.get('BOT_USERNAME', 'kmdorgbot')
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'https://shareus.io/')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'PPrfEdbYHDaV3GFqRvvtGy712nj1')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', '')
+SHORTLINK_API = environ.get('SHORTLINK_API', '')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
